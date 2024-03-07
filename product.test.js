1e6311687aaebe2273ae5ee3595ad7ce036b929e
Jeannie T <98559350+jstays@users.noreply.github.com>
2024-03-06 20:24:17 -0600

    Update product.test.js
    Fixed duplicate error message and missing semicolon.
    Made comments more cohesive.

--- product.test.js
+++ product.test.js
@@ -1,28 +1,27 @@
 
-// Basic test function
+// Basic test function, throw error on test failure.
 function test(testCondition, expected) {
     if (!expected) {
         throw new Error('Test failed: ' + expected);
     }
 }
 
-// Unit tests for product function
+// Unit tests for product function.
 function testProduct() {
     // Test known product with string input.
     test(product('Heinz Ketchup') === 'Heinz', 'Heinz Ketchup is owned by Heinz.');
 
-    // Test known product with UPC int input
+    // Test known product with UPC input.
     test(product('27896102000409') === 'Heinz', 'UPC 27896102000409 is owned by Heinz.');
 
-    // Test known non-food product
-    test(product('000000000000') === 'The UPC you have entered belongs to a non-food item.', 'UPC 000000000000 is a non-food item.')
+    // Test with known non-food product.
+    test(product('000000000000') === 'The UPC you have entered belongs to a non-food item.', 'UPC 000000000000 is a non-food item.');
 }
 
-// Try/Catch for running tests
+// Try to run tests, output pass notification if completed, else output error notification.
 try {
     testProduct();
     console.log('All tests passed.');
-}
-catch (error) {
-    console.error('Test failed: ' + error.message);
+} catch (error) {
+    console.error(error.message);
 }
