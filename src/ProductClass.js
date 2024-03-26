bf4488ae1c2ab0fed08281c5eed6a2a1e0e21fa4
jstays <jeannie.m.taylor@ou.edu>
2024-03-26 14:06:58 -0500

    added setter documentation

--- src/ProductClass.js
+++ src/ProductClass.js
@@ -10,14 +10,26 @@ class Product{
         this.productCompany = company;
     }
 
+    /** 
+     * Sets Product name.
+     * @param {*} name new product name
+     */
     setProductName(name){
         this.productName = name;
     }
 
+    /** 
+     * Sets Product UPC.
+     * @param {*} UPC new product UPC identifier
+     */
     setProductUPC(UPC){
         this.productUPC = UPC;
     }
 
+    /**
+     * Sets Product company.
+     * @param {*} company new Product manufacturer name
+     */
     setProductCompany(company){
         this.productCompany = company;
     }
