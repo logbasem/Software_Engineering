15259c9234d7eab95afe3f8748427c02a4a35c72
jstays <jeannie.m.taylor@ou.edu>
2024-03-26 13:46:14 -0500

    setup product constructor

--- src/ProductClass.js
+++ src/ProductClass.js
@@ -1,3 +1,12 @@
 class Product{
 
+    productName;
+    productUPC;
+    productCompany;
+    
+    Product(name, UPC, company){
+        this.productName = name;
+        this.productUPC = UPC;
+        this.productCompany = company;
+    }
 }
