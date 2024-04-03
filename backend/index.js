41dce87ac5f21b748df2d465344d5412a70d067e
Kpeters <kaitlynpeters@cox.net>
2024-04-02 21:02:21 -0500

    basic backend + updated readme for npm start command when starting the backend

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,23 @@
 
 //node js and express js to connect to mySQL database
 import express from "express"
+import mysql from "mysql"
 
 const app = express()
 
+//connect to db
+//todo: make this flexible with everyone's local db (create .env file/db config file?)
+// these are the credentials of my local SQL bench but i'll make sure to update this soon.
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"root",
+    database:"kyper"
+})
+
+app.get("/", (req, res) => {
+    res.json("this is the backend")
+})
 app.listen(3000, ()=>{
     console.log("Connected to backend!")
 })
