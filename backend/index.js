146f7bfe6d9b5beb502e5b09d615e5d9c850937a
Kpeters <kaitlynpeters@cox.net>
2024-04-02 21:37:45 -0500

    added .env file and dotenv library so that users can configure their local db settings

--- backend/index.js
+++ backend/index.js
@@ -2,24 +2,27 @@
 // Kaitlyn Peters
 
 //node js and express js to connect to mySQL database
-import express from "express"
-import mysql from "mysql"
+import express from "express";
+import mysql from "mysql";
+import dotenv from "dotenv";
 
-const app = express()
+const app = express();
+//get environment variables from .env file
+dotenv.config();
 
-//connect to db
-//todo: make this flexible with everyone's local db (create .env file/db config file?)
-// these are the credentials of my local SQL bench but i'll make sure to update this soon.
+//open db connection
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"root",
-    database:"kyper"
-})
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
 
+//home page route connected to the backend
 app.get("/", (req, res) => {
     res.json("this is the backend")
-})
+});
+
 app.listen(3000, ()=>{
     console.log("Connected to backend!")
-})
+});
