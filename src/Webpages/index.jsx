0f7c0ba35f3a6695feafef40cb724bcab09ce563
jstays <jeannie.m.taylor@ou.edu>
2024-04-04 16:31:32 -0500

    Fully Switched to React

---
+++ src/Webpages/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { Homepage } from 'Homepage.jsx'
+
+ReactDOM.createRoot( 
+  document.querySelector('#root')
+).render(<Homepage />)
