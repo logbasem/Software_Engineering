eca30caf145f3326272221abc2b5594f19014f61
Jeannie T <jeannie.m.taylor@ou.edu>
2024-04-15 17:20:13 -0500

    Routed Profile to /profile Routed Login to /login Routed Register to /register Fixed broken css link on LoginRegister Fixed missing react-hook-form package

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Homepage from './pages/Homepage';
 import Profile from './pages/Profile';
+import { Login } from './pages/LoginRegister';
+import { Register } from './pages/LoginRegister';
 
 function App() {
   return (
@@ -10,6 +12,10 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Homepage />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
         </Routes>
         {/*<Routes>
           <Route path="/" element={<SOME OTHER ROUTE ETC.... />} />
