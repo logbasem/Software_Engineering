56954810dd3ded8ac6a6c260e30408727fd11101
Brianna Patten <brianna.patten@ou.edu>
2024-04-16 17:16:42 -0500

    Make buttons reactive on profile page

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<Profile />} />
+          <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
